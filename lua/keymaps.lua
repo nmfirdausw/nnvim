c2b4f87ee7e52751f8aada0e402d9f76a579d8a1
nmfirdausw <nmfirdausw@gmail.com>
2022-09-26 07:34:26 +0800

    Neo-tree keymapping

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,1 +1,5 @@
 local set = vim.keymap.set
+
+-- Neo-tree File Explorer
+set('n', '<leader>e', '<cmd>Neotree reveal<cr>', { desc = 'Focus file Explorer' })
+set('n', '<leader>te', '<cmd>Neotree toggle<cr>', { desc = 'File Explorer' })
