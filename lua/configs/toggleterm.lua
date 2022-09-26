29906482c0183ded76f011ef3d5180bbf0494726
nmfirdausw <nmfirdausw@gmail.com>
2022-09-26 23:46:27 +0800

    remove mappings

--- lua/configs/toggleterm.lua
+++ lua/configs/toggleterm.lua
@@ -2,11 +2,10 @@ local toggleterm_exists, toggleterm = pcall(require, "toggleterm")
 if not toggleterm_exists then return end
 toggleterm.setup({
   size = 10,
-  open_mapping = [[<c-\>]],
   shading_factor = 2,
   direction = "float",
   float_opts = {
-    border = "curved",
+    border = "single",
     highlights = {
       border = "Normal",
       background = "Normal",
