71b7041882440cc9963558faa50232b30f934f0c
nmfirdausw <nmfirdausw@gmail.com>
2022-09-26 23:49:52 +0800

    toggleterm keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -9,3 +9,8 @@ set('n', '<C-h>', function() require('smart-splits').move_cursor_left() end, { d
 set('n', '<C-j>', function() require('smart-splits').move_cursor_down() end, { desc = 'Move to left down' })
 set('n', '<C-k>', function() require('smart-splits').move_cursor_up() end, { desc = 'Move to left up' })
 set('n', '<C-l>', function() require('smart-splits').move_cursor_right() end, { desc = 'Move to left right' })
+
+-- Terminal
+set('n', '<leader>tf', '<cmd>ToggleTerm direction=float<cr>', { desc = 'ToggleTerm float' })
+set('n', '<leader>th', '<cmd>ToggleTerm size=10 direction=horizontal<cr>', { desc = 'ToggleTerm horizontal split' })
+set('n', '<leader>tv', '<cmd>ToggleTerm size=40 direction=vertical<cr>', { desc = 'ToggleTerm vertical split' })
